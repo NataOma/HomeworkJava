1394056db56212871781e35a6b1ae389d97e1aea
Natalia <chekiska.11@mail.ru>
2024-09-25 15:40:06 +0500

    Задача на Вывод всех четных чисел от 1 до 100, правка2

--- ht2.java
+++ ht2.java
@@ -1,5 +1,9 @@
 package HomeWork;
 
+// Вывод всех четных чисел от 1 до 100
+// Напишите метод printEvenNums, который выведет на экран все четные числа
+// в промежутке от 1 до 100, каждое на новой строке
+
 public class ht2 {
 
     public static void main(String[] args) {
