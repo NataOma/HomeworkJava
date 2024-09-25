7599e25735ae059f56557be4cd121737528c9581
Natalia <chekiska.11@mail.ru>
2024-09-25 15:38:13 +0500

    Задача на Вывод всех четных чисел от 1 до 100

---
+++ ht2.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+public class ht2 {
+
+    public static void main(String[] args) {
+
+        printEvenNums();
+    }
+
+    public static void printEvenNums() {
+        
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println(i);
+            
+
+        }
+    
+    }
+}
